d7caca6826e66f02275395afcc48e05a1c4c9118
Andres Matta Morales <andres.sm123@gmail.com>
2018-12-27 04:03:11 -0600

    Initial Setup & Database Seeding

---
+++ database/migrations/2018_12_27_093854_create_replies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('body');
+            $table->unsignedInteger('thread_id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+            //Foreign Keys.
+            $table->foreign('thread_id')->references('id')->on('threads')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
